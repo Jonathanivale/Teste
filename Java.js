11b90c0f23476042ab11e750353dc9ba1df56377
Jonathan Ivale <85253332+Jonathanivale@users.noreply.github.com>
2022-08-10 13:17:54 -0300

    Create Java.js

---
+++ Java.js
@@ -0,0 +1,23 @@
+public class main{
+Public static void main (string [] args ){
+
+    Scanner input = new scanner(system.in);
+
+    System.out.println("Digite o total de mercadorias em estoque: ");
+    int totalProducts = 0;
+
+    for (int counter = 1; counter <= totalProducts; counter++) {
+             system.out.println("informe o valor da mercadoria " + counter + ': ');
+             totalProductsvalue += input.nextDouble();
+
+
+    }
+
+         system.out.println("valor total das mercadorias: " + totalProductsvalue);
+         system.out.println("Média dos valores: " + (totalProducts / totalProductsvalue));
+
+
+
+}
+
+}
